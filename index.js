417fd43562f5c30e4e187862065ec9c8cfcb530b
coder-yeasin <117996077+coder-yeasin@users.noreply.github.com>
2022-11-12 16:40:55 +0600

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,11 @@
     
     
 //git init 
-
- 
-
-
+// echo "# Authentication-with-mongobd" >> README.md
+// git init
+// git add README.md   / git add .
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/coder-yeasin/Authentication-with-mongobd.git
+// git push -u origin main
 
